5e14d20f74e2df4133c143585e387d6327c4e4b1
goutam.a.gupta <goutam.a.gupta@M7-DX-HTZQXJ2.dir.svc.accenture.com>
2019-08-08 16:08:25 +0530

    tesng xml added.

--- Cucumber/src/main/java/Cucumber/RunnerClass.java
+++ Cucumber/src/main/java/Cucumber/RunnerClass.java
@@ -4,7 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(features = "resources", glue = "Cucumber", monochrome = true, plugin = { "pretty",
-		"html:cucumber-html-report" }, tags = { "@Login" }, dryRun = false)
+		"json:target/cucumber.json" }, tags = { "@Login" }, dryRun = false)
 
 public class RunnerClass extends AbstractTestNGCucumberTests {
 
